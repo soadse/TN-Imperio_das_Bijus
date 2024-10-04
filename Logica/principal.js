40b21d21099bae002947b8c997357dd092746aaa
Rodrigo Andrade <soadse.ra@gmail.com>
2024-10-03 21:34:41 -0300

    teste

---
+++ Logica/principal.js
@@ -0,0 +1,24 @@
+let menuOpen = window.document.querySelector('.menuOpen')
+let menuClose = window.document.querySelector('.menuClose')
+let menu01 = window.document.querySelector('.menu01')
+let menu02 = window.document.querySelector('.menu02')
+let menu03 = window.document.querySelector('.menu03')
+let menu04 = window.document.querySelector('.menu04')
+
+
+function abreMenu(){
+    menuOpen.style.display = 'none'
+    menuClose.style.display = 'flex'
+    menu01.style.display = 'flex'
+    menu02.style.display = 'flex'
+    menu03.style.display = 'flex'
+    menu04.style.display = 'flex'
+}
+function fechaMenu(){
+    menuOpen.style.display = 'flex'
+    menuClose.style.display = 'none'
+    menu01.style.display = 'none'
+    menu02.style.display = 'none'
+    menu03.style.display = 'none'
+    menu04.style.display = 'none'
+}
